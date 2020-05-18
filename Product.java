9859ab8576ebecde5ad3316ccfad95beef969b30
Evelynn Geisinger <evelynn.geisinger@gmail.com>
2020-05-18 13:31:36 +0200

    Add Product

---
+++ Product.java
@@ -0,0 +1,19 @@
+public class Product
+{
+  private String name = "Coca Cola";
+
+  public Product()
+  {
+
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+}
